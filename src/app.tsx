c91786220d7103ceaddcf0a34987e86723ac51a6
Sebb-Cipher <129906074+Sebb-Cipher@users.noreply.github.com>
2024-09-01 18:39:26 -0400

    a

--- src/app.tsx
+++ src/app.tsx
@@ -4,13 +4,6 @@ function App() {
   return (
     <div>
       <Login />
-
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsa
-        incidunt temporibus provident qui, dicta beatae maiores iusto nesciunt
-        optio porro earum quas fugit mollitia tenetur sapiente! Temporibus, aut
-        officia?
-      </h1>
     </div>
   );
 }
