c72a8ea35bf4896becb4ee501079e86c526b7b12
Sebb-Cipher <129906074+Sebb-Cipher@users.noreply.github.com>
2024-09-02 13:52:01 -0400

    commit

--- src/Components/login.tsx
+++ src/Components/login.tsx
@@ -4,19 +4,56 @@ import React from "react"; // eslint-disable-next-line no-unused-vars
 function login() {
   return (
     <React.Fragment>
-      <div className="login-container">
-        <h2>Login</h2>
+      <div className="login-contianer">
+        <h2 className="login-title"> Sign in to your Account</h2>
+        <p className="login-subtitle">
+          Welcome back! Please enter your details
+        </p>
         <form action="">
-          <input type="text" placeholder="Username" className="login-input" />
-          <input
-            type="password"
-            placeholder="Password"
-            className="login-input"
-          />
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Email Address"
+              className="login-input"
+            />
+            <span className="input-icon">
+              <i className="uil uil-envelope"></i>
+            </span>
+          </div>
+          <div className="input-group">
+            <input
+              type="passowrd"
+              placeholder="Password"
+              className="login-input"
+            />
+            <span className="input-icon">
+              <i className="uil uil-lock-alt"></i>
+            </span>
+          </div>
+          <div className="login-options">
+            <div className="remember-me">
+              <input type="checkbox" id="remember" />
+              <label htmlFor="remmber">Remember Me</label>
+            </div>
+            <a href="#" className="forgot-password">
+              Forgot Passowrd
+            </a>
+          </div>
+
           <button type="submit" className="login-button">
-            Login
+            Sign In
+          </button>
+          <div className="divider">
+            <span>Or sign in with</span>
+          </div>
+          <button type="button" className="google-button">
+            <img src="" alt="Google logo" />
+            Google
           </button>
         </form>
+        <p className="signup-link">
+          Don't have an account <a href="@">Sign Up </a>
+        </p>
       </div>
     </React.Fragment>
   );
