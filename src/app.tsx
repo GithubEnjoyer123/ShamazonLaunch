000f55a5a8ccd114525920eaa67e21f8c36468bc
Sebb-Cipher <129906074+Sebb-Cipher@users.noreply.github.com>
2024-09-20 23:25:09 -0400

    Navigation to Landing Page

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import router components
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"; // Import router components
 import Login from "./Components/login"; // Import Login component
 import Navbar from "./Components/navbar"; // Import Navbar component
 import "./app.css"; // Import styles
@@ -9,6 +14,9 @@ function App() {
     <Router>
       <Routes>
         {/* Define separate routes for each page  Routing process for which page would be the landing page */}
+        <Route path="/" element={<Navigate to="/navbar" />} />
+        {/* The line (ln 17) above allows the webpage to go a specific landing page, for DEV stage's it would be navbar */}
+
         <Route path="/login" element={<Login />} />
         <Route path="/navbar" element={<Navbar />} />
       </Routes>
